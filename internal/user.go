fc55892230c4a0201b3b08c4f1a37b4191646acc
Gabriel Demesio <cfmoneygamer@gmail.com>
2025-03-17 14:09:08 -0300

    criando testes

--- internal/user.go
+++ internal/user.go
@@ -1,15 +1,16 @@
 package internal
 
 import (
+	"FULLCYCLE/pkg/entity"
 	"golang.org/x/crypto/bcrypt"
 	_ "golang.org/x/crypto/bcrypt"
 )
 
 type User struct {
-	ID       string `json:"id"`
-	Name     string `json:"name"`
-	Email    string `json:"email"`
-	Password string `json:"-"`
+	ID       entity.ID `json:"id"`
+	Name     string    `json:"name"`
+	Email    string    `json:"email"`
+	Password string    `json:"-"`
 }
 
 func NewUser(name, email, password string) (*User, error) {
@@ -18,7 +19,7 @@ func NewUser(name, email, password string) (*User, error) {
 		return nil, err
 	}
 	return &User{
-		ID:       "id",
+		ID:       entity.NewID(),
 		Name:     name,
 		Email:    email,
 		Password: string(hash),
